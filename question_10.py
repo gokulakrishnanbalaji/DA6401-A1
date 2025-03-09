efee5349a360293d8dd9235095a2488c97542d94
Gokulakrishnan <gokulbalaji2408@gmail.com>
2025-03-10 04:42:41 +0530

    updated relu to ReLU

--- question_10.py
+++ question_10.py
@@ -9,9 +9,9 @@ from config import entity, project
 # (relu, momentum ) ,(tanh, nadam), (tanh,momentum)
 def compare_models(epochs,batch_size):
     wandb.init(project=project, entity=entity)
-    model_1 = AdvancedFFNN(num_layers=5,hidden_size=128, learning_rate=1e-3, optimizer='momentum', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='relu')
-    model_2 = AdvancedFFNN(num_layers=5,hidden_size=128, learning_rate=1e-3, optimizer='nadam', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='tanh')
-    model_3 = AdvancedFFNN(num_layers=5,hidden_size=128, learning_rate=1e-3, optimizer='momentum', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='tanh')
+    model_1 = AdvancedFFNN(num_layers=5,hidden_size=256, learning_rate=1e-3, optimizer='momentum', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='ReLU')
+    model_2 = AdvancedFFNN(num_layers=5,hidden_size=256, learning_rate=1e-3, optimizer='nadam', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='tanh')
+    model_3 = AdvancedFFNN(num_layers=5,hidden_size=256, learning_rate=1e-3, optimizer='momentum', weight_decay=0, batch_size=batch_size, weight_init='xavier', activation='tanh')
 
     # running each of the model for 10 epochs
     model_1.train(X_train, y_train,X_val,y_val,X_test, y_test, epochs)
